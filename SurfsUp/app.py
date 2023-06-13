f32e28db3ca280dcdf8c11862f3a1948b438a8be
Daniel Murillo <danielmgiant@gmail.com>
2023-06-13 02:08:49 -0600

    Uploaded all the static routes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -4,12 +4,14 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+import numpy as np
+import pandas as pd
+import datetime as dt
 #from flask_cors import CORS
 
 #################################################
 # Database Setup
 #################################################
-
 engine = create_engine('sqlite:///Resources/hawaii.sqlite')
 
 # reflect an existing database into a new model
@@ -24,6 +26,13 @@ measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
+
+# Defined a function to find the year before; the function is used in the precipitation and temperature routes:
+def the_year_before():
+    most_recent_datapoint = session.query(func.max(measurement.date)).scalar()
+    reformatted_most_recent_datapoint = dt.datetime.strptime(most_recent_datapoint, '%Y-%m-%d').date()
+    one_year_before = reformatted_most_recent_datapoint - dt.timedelta(days=365)
+    return (one_year_before)
 
 #################################################
 # Flask Setup
@@ -54,14 +63,39 @@ def homepage():
 # Precipitation query route:
 @app.route('/api/v1.0/precipitation')
 def precipitation_query():
-    most_recent_datapoint = session.query(func.max(measurement.date)).scalar()
-    reformatted_most_recent_datapoint = dt.datetime.strptime(most_recent_datapoint, '%Y-%m-%d').date()
-    one_year_before = reformatted_most_recent_datapoint - dt.timedelta(days=365)
-    last_year_precipitation_scores_l = session.query(measurement.date, measurement.prcp).filter(measurement.date > one_year_before).all()
+    last_year_precipitation_scores_l = session.query(measurement.date, measurement.prcp).filter(measurement.date > the_year_before()).all()
+    last_year_precipitation_scores_d = dict(last_year_precipitation_scores_l)
+    session.close()
+    return jsonify(last_year_precipitation_scores_d)
     
+# Stations query route:
+@app.route('/api/v1.0/stations')
+def stations_query():
+    station = Base.classes.station
+    stations_l = session.query(station.id, station.station, station.name, station.latitude, station.longitude, station.elevation).all()
+    stations_d = []
+    for id, station, name, latitude, longitude, elevation in stations_l:
+        stations_temp_d = {}
+        stations_temp_d['id'] = id
+        stations_temp_d['station'] = station
+        stations_temp_d['name'] = name
+        stations_temp_d['latitude'] = latitude
+        stations_temp_d['longitude'] = longitude
+        stations_temp_d['elevation'] = elevation
+        stations_d.append(stations_temp_d)
+    session.close()
+    return jsonify(stations_d)
 
-
-
+# Temperatures query route:
+@app.route('/api/v1.0/tobs')
+def temperatures_query():
+    most_active_stations_a = session.query(measurement.station, func.count(measurement.date)).group_by(measurement.station).order_by(func.sum(measurement.station).desc()).all()
+    sorted_most_active_stations_a = sorted(most_active_stations_a, key=lambda x: -x[1])
+    most_active_station = sorted_most_active_stations_a[0][0]
+    last_year_temperature_data_most_active_station_l = session.query(measurement.date, measurement.tobs).filter(measurement.date > the_year_before()).filter(measurement.station == most_active_station).all()
+    last_year_temperature_data_most_active_station_d = dict(last_year_temperature_data_most_active_station_l)
+    session.close()
+    return(last_year_temperature_data_most_active_station_d)
 
 
 
